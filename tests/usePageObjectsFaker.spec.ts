e4e772ba4f17569db40e9492bcb649944d0c363b
JoaoPires92 <joao.npires92@gmail.com>
2024-04-22 21:52:46 +0100

    update

--- tests/usePageObjectsFaker.spec.ts
+++ tests/usePageObjectsFaker.spec.ts
@@ -1,5 +1,6 @@
-import { test} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 import { PageManager } from '../page-objects/pageManager'
+import { faker } from '@faker-js/faker' // Install by runnning command: npm i @faker-js/faker --save-dev --force
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
@@ -14,9 +15,13 @@ test('navigate to page', async ({ page }) => {
 
 test('parametrized methods', async ({ page }) => {
     const pm = new PageManager(page)
+
+    const randomFullName = faker.person.fullName()
+    const randomEmail = `${randomFullName.replace(' ', '')}${faker.number.int(1000)}@test.com`
+
     await pm.navigateTo().formLayoutsPage()
     await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption('joaopires92@test.com', 'Password99', 'Option 2')
-    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox('Joao Pires', 'joaopires92@test.com', true)
+    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox(randomFullName, randomEmail, false)
     await pm.navigateTo().datepickerPage()
     await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(10)
     await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 15)
